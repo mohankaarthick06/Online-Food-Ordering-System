9ff0ba6180ad4a691b2a4778a4756e3fe58599c0
mohankaarthick06 <mohankaarthick06@gmail.com>
2025-09-15 10:29:59 +0530

    user details

---
+++ src/model/User.java
@@ -0,0 +1,21 @@
+
+
+public abstract class User {
+    private final int id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
