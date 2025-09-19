765c003210d308e4120c0c96cd63a2df81dceffc
Mohan <mohankaarthick06@gmail.com>
2025-09-19 20:38:35 +0530

    map for top selling items

--- src/util/ReportService.java
+++ src/util/ReportService.java
@@ -1,5 +1,7 @@
-package util;
+import model.Order;
+import java.util.*;
 
 public class ReportService {
-    
-}
+    /* Find top selling items */
+    public Map<String,Integer> topSellingItems(Collection<Order> orders, int topN) {
+        Map<String,Integer> counts = new HashMap<>();
