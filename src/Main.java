559c91168c7e7dccb8e2e438f1f3fddc2b88a496
Mohan <mohankaarthick06@gmail.com>
2025-09-26 09:44:35 +0530

    updating main function based on the structure

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,14 @@ public class Main {
         FoodOrderingService service = new FoodOrderingService();
         
         ReportService reportService = new ReportService();
+        
+        Restaurant r1 = service.createRestaurant("Pizza Hut", "Chennai");
+
+        service.addMenuItem(r1, "Margherita Pizza", "Classic cheese pizza", 299);
+
+        service.addMenuItem(r1, "Veggie Burger", "Burger with fresh veggies", 149);
+
+        service.addMenuItem(r1, "Coke", "Cold drink", 49);
 
         
        
